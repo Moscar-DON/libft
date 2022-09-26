0f75cc02e80cf4253b0591e8e22178e11f591cce
Oscar Perez De Bustos Diaz-Flores <operez-d@c2r11s5.42madrid.com>
2022-09-26 16:56:47 +0200

    libft 5

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,13 @@
 /*   By: operez-d <operez-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 17:07:31 by operez-d          #+#    #+#             */
-/*   Updated: 2022/09/26 13:56:52 by operez-d         ###   ########.fr       */
+/*   Updated: 2022/09/26 16:54:01 by operez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*char	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
 	int		start;
@@ -20,46 +20,17 @@
 	
 	if (!s1 || !set)
 		return (0);
-	str = ft_calloc(ft_strlen((char *)s1)+ 1, sizeof(char));
-	if (!str)
-		return (0);
 	start = 0;
-	while (s1 && !ft_strchr(set, s1[start]))
+	end = ft_strlen((char *)s1) - 1;
+	while (s1 && ft_strchr(set, s1[start]))
 		start++;
-	
-}*/
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*str;
-	size_t	i;
-	size_t	j;
-	size_t	k;
-	int		rep;
-	
-	if (!s1 || !set)
-		return (0);
-	
-	str = ft_calloc(ft_strlen((char *)s1)+ 1, sizeof(char));
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	str = ft_substr(s1, start, end - start + 1);
 	if (!str)
 		return (0);
-	i = 0;
-	k = 0;
-	rep = 0;
-	while (s1[i])
-	{
-		j = -1;
-		while (set[++j])
-		{
-			if (s1[i] == set[j])
-				rep = 1;
-		}
-		if (rep == 0)
-			str[k++] = (char)s1[i];
-		i++;
-		rep = 0;
-	}
 	return (str);
+	
 }
 /*
 int	main()
